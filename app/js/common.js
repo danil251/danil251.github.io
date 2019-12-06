0f7aa2ef619796c294eb7d4b23c28a5a21afddd7
danil251 <slipitout@yandex.ru>
2019-12-07 01:26:06 +0500

    inital commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,6 @@
 
 	function showContent () {
-		let contentTitle = document.querySelectorAll('.block .title');
+		let contentTitle = document.querySelectorAll('.faq__right-block .faq__right-block-title');
 
 		for (let i = 0; i < contentTitle.length; i++){
 			contentTitle[i].addEventListener('click', function(e){
