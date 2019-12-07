18ca51cc74c2ccc44eb70a8e98cb2c6f24524cf1
danil251 <slipitout@yandex.ru>
2019-12-08 00:47:54 +0500

    inital commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,16 +1,31 @@
 
-	function showContent () {
-		let contentTitle = document.querySelectorAll('.faq__right-block .faq__right-block-title');
+const anchors = document.querySelectorAll('a[href*="#"]')
 
-		for (let i = 0; i < contentTitle.length; i++){
-			contentTitle[i].addEventListener('click', function(e){
-				if (e.target.classList.contains('active')) {
-					this.classList.remove('active');
-				} else {
-					this.classList.add('active');
-				}	
+for (let anchor of anchors)	{
+	anchor.addEventListener("click", function(event) {
+		event.preventDefault();
+		const blockID = anchor.getAttribute('href')
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: "smooth",
+			block: "start"
+		})
+	})
+
+}
+
+
+function showContent () {
+	let contentTitle = document.querySelectorAll('.faq__right-block .faq__right-block-title');
+
+	for (let i = 0; i < contentTitle.length; i++){
+		contentTitle[i].addEventListener('click', function(e){
+			if (e.target.classList.contains('active')) {
+				this.classList.remove('active');
+			} else {
+				this.classList.add('active');
 			}	
-			)}
-		}
-		showContent();
+		}	
+		)}
+	}
+	showContent();
 
